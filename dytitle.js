d1ba77ade740b494e511267b1ea433496aecabd2
phantomfrost <78469955+phantomfrost@users.noreply.github.com>
2021-08-07 22:00:25 +0800

    Update dytitle.js

--- dytitle.js
+++ dytitle.js
@@ -3,12 +3,10 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/TRHX/CDN-for-itrhx.com@3.0.4/images/funny.ico");
         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/TRHX/CDN-for-itrhx.com@3.0.4/images/favicon.ico");
         document.title = '(ฅ>ω<*ฅ) 噫又好啦 ~' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
